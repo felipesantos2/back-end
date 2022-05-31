5e80731b6912a87fd8f1d5ea6b387241b4875cef
Felipe pinheiro dos Santos <santospinheiro627@gmail.com>
2022-05-31 10:31:21 -0300

    Update novo_usuario.php

--- crud/novo_usuario.php
+++ crud/novo_usuario.php
@@ -1,26 +1,34 @@
-<h1>Novo Usuario</h1>
+<style>
+    .form-control {
+        width: 40%;
+        display: block;
+    }
+    
+</style>
 
 <form action="?page=salvar" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
-    <div class="mb-3 d-flex justify-content-center">
-        <label>nome:</label>
-        <input type="text" class="form-control-sm" placeholder="nome" name="text_name">
-    </div>
-    <div class="mb-3 d-flex justify-content-center">
-        <label>email:</label>
-        <input type="email" class="form-control-sm" placeholder="E-mail" name="text_email">
-    </div>
-    <div class="mb-3 d-flex justify-content-center">
-        <label>senha:</label>
-        <input type="password" class="form-control-sm" placeholder="Senha" name="t_senha">
-    </div>
-    <div class="mb-3 d-flex justify-content-center">
-        <label>data de nascimento:</label>
-        <input type="date" class="form-control-sm" placeholder="Data de Nascimento" name="t_data_nasc">
-    </div>
-    <div class="mb-3 d-flex justify-content-center">
-        <input type="submit" class="btn btn-primary" value="Enviar">
-    </div>
+    <div class="form-content align-items-center justify-content-center">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nome:</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="text_name">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Email:</label>
+            <input type="email" class="form-control" id="exampleInputPassword1" name="text_email">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha:</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" name="t_senha">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha:</label>
+            <input type="date" class="form-control" id="exampleInputPassword1" name="t_data_nasc">
+        </div>
 
-
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1" name="t_data_nasc">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
 </form>
