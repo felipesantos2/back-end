048e50801b17230000517945a83044caccbd023b
Felipe <santospinheiro627@gmail.com>
2022-06-04 16:58:46 -0300

    Update processa.php

--- formulario_contato/processa.php
+++ formulario_contato/processa.php
@@ -1,38 +1,32 @@
-<style>
-    span {
-        font-size: 1.1rem;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        color: red;
-    }
-</style>
 <?php
+$erroN = $erroE = $erroM = "";
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = @$_POST['nome'];
     $email = @$_POST['email'];
     $mensagem  = @$_POST['mensagem'];
-} else {
-    echo "<span>Falha no metodo de requisição<span>";
-}
+} #else {
+# echo "<span>Falha no metodo de requisição<span>";}
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
     if ($nome == '') {
         // echo '<script>alert("Preencha os campos por favor")</script>';
-        echo "<span>Preencha por favor o campo NOME</span><br>";
+        // echo "<span>Preencha por favor o campo NOME</span><br>";
+        $erroN = "<span>Preencha por favor o campo NOME</span><br>";
     }
-    if ( $email == '') {
+    if ($email == '') {
         // echo '<script>alert("preencha os campos por favor")</script>';
-        echo "<span>preencha por favor  o Campo E-MAIL</span><br>";
+        // echo "<span>preencha por favor  o Campo E-MAIL</span><br>";
+        $erroE = "<span>Preencha por favor  o Campo E-MAIL</span><br>";
     }
     if ($mensagem == '') {
         // echo '<script>alert("preencha os campos por favor")</script>';
-        echo "<span>preencha por favor a MENSAGEM</span><br>";
-        // $erro = "<span>Errado</span>";
+        // echo "<span>preencha por favor a MENSAGEM</span><br>";
+        $erroM = "<span>Preencha por favor a MENSAGEM</span><br>";
     }
-
-} 
+}
 
 
 ?>
