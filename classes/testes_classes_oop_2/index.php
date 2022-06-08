145cbf1a4ca49a898eebb96e43f6dbdd6802ea8e
Felipe <santospinheiro627@gmail.com>
2022-06-07 22:13:31 -0300

    Big bang

--- classes/testes_classes_oop_2/index.php
+++ classes/testes_classes_oop_2/index.php
@@ -2,9 +2,29 @@
 
 
 
-class Carro_ano {
-    moode
+class Carro{
+    //propriedades
+    public $modelo;
+    public $cor;
 
+    //Métodos
+    function set_modelo($modelo){
+        $this->modelo = $modelo;
 
+    }
 
+    function get_modelo(){
+        return $this->modelo;
+    }
+
+  
 }
+
+    $carro1 = new Carro();
+
+    var_dump($uno);
+
+    $carro1->set_modelo("Uno");
+
+    var_dump($uno);
+
